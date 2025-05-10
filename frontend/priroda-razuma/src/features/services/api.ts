fc5467927c775263a0fea00437358845fda83f36
slsforme <27ofdecember@gmail.com>
2025-05-10 18:53:35 +0300

    [FIX] HTTPS PROB (api)

--- frontend/priroda-razuma/src/features/services/api.ts
+++ frontend/priroda-razuma/src/features/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://prirodarazumadev.ru/api/v1"  
+baseURL: "https://prirodarazumadev.ru/api/v1"  
 });
 
 api.interceptors.request.use(
